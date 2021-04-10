9de1e93da691750535dca910c4b128f6944629e1
krishtheavenge <75679477+krishtheavenge@users.noreply.github.com>
2021-04-10 09:38:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 function setup(){
- var  canvas = createCanvas(640,480);
- canvas.center();
- video=createCapture(VIDEO);
- video.hide();
-
-}
-
-function draw(){
-    image(video,230,150,220,200);
-
-
-    fill(0,100,128);
-    stroke(0,100,128);
-    rect(90,40,460,20);
-
-    rect(90,420,460,20);
-    rect(90,40,20,400);
-    rect(550,40,20,400);
-}
-
-
-function snap(){
-    save("mypic.png");
-}
+    var  canvas = createCanvas(640,480);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+   
+   }
+   
+   function draw(){
+       image(video,230,150,220,200);
+   
+   
+       fill(0,100,128);
+       stroke(0,100,128);
+       rect(90,40,460,20);
+   
+       rect(90,420,460,20);
+       rect(90,40,20,400);
+       rect(550,40,20,400);
+   }
+   
+   
+   function snap(){
+       save("mypic.png");
+   }
